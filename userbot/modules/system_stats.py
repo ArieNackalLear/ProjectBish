466a1c7d778a7add20ff1ac944f2d594ebef652a
ArieNackalLear <62494146+ArieNackalLear@users.noreply.github.com>
2020-04-12 00:05:02 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -123,13 +123,14 @@ async def pipcheck(pip):
 async def amireallyalive(alive):
     """ For .on command, check if the bot is running.  """
     await alive.edit("`"
-                     "I'm alive dude....\n"
-                     f"===================================================\n"
-                     f"•User             : {DEFAULTUSER}\n"
-                     f"•Bio              :  ɪ'ᴍ ɴᴏᴛ ʜᴇʀᴇ, ʙᴇᴄᴀᴜꜱᴇ ɪ ᴅᴇᴀᴅ ..!\n"
-                     f"•Python           : {python_version()}\n"
-                     f"•Telethon version : {version.__version__}\n"
-                     f"====================================================\n"
+                     "I'm on my way ....\n" 
+                     f"==============================================\n"
+                     f"> User             : {DEFAULTUSER}\n"
+                     f"> Bio              : ɪ'ᴍ ɴᴏᴛ ʜᴇʀᴇ,ʙᴇᴄᴀᴜꜱᴇ ɪ ᴅᴇᴀᴅ!\n"
+                     f"> Python           : {python_version()}\n"
+                     f"> Telethon version : {version.__version__}\n"
+                     f"==============================================\n"
+                     f" Type .help for more information!\n"
                      "`")
 
 
